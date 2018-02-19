0267b7655150561b083981518153c21f11f7ec48
DESKTOP-LA12PN9\Samaarambh <raghav.samaarambh@gmail.com>
2018-02-19 17:12:09 +0530

    controller updated for dynamic form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
@@ -23,8 +26,25 @@ class DefaultController extends Controller
     /**
      * @Route("/main", name="mainPage")
      */
-    public function home(){
+    public function home(Request $request){
         return $this->render('default/home.html.twig');
+
+    }
+
+
+    /**
+     * @Route("/main_custom", name="mainCustomPage")
+     */
+    public function home_custom(){
+
+        $form = $this->createFromBuilder()
+                ->add('task',TextType::class)
+                ->add('name',TextType::class)
+                ->getForm();
+
+        return $this->render('default/home_custom.html.twig', array(
+            'form'=>    $form->createView(),
+        ));
 
     }
 
